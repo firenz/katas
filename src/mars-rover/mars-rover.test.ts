8b0a469144fabb9f0edcab843a141e63d72fbc3f
Alicia Guardeño <alicia.guardeno@qatium.com>
2022-11-30 14:17:33 +0100

    ❌ 1669814253

--- src/mars-rover/mars-rover.test.ts
+++ src/mars-rover/mars-rover.test.ts
@@ -1,15 +1,6 @@
 import { MarsRover, Direction } from "./mars-rover";
 
 describe("Mars Rover kata", () => {
-  it("is given an starting position", () => {
-    const positionX = 1;
-    const positionY = 1;
-    const marsRover = new MarsRover(positionX, positionY);
-
-    expect(marsRover.position.x).toBe(1);
-    expect(marsRover.position.y).toBe(1);
-  });
-
   it("has a starting position by default", () => {
     const marsRover = new MarsRover();
 
