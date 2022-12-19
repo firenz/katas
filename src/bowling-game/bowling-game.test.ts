2f4c039fdbb6e8467171ed3e57a0326073e0ee57
Alicia Guardeño <alicia.guardeno@qatium.com>
2022-12-19 14:46:12 +0100

    ❌ 1671457572
    /usr/local/bin/katatracker: line 26: test-watch: command not found

--- src/bowling-game/bowling-game.test.ts
+++ src/bowling-game/bowling-game.test.ts
@@ -2,21 +2,15 @@ import { BowlingGame } from "./bowling-game";
 
 
 describe("Bowling game kata", () => {
-  it("counts pins down in a roll", () => {
-    const pinsDown = 6;
-    const bowlingGame = new BowlingGame();
-
-    bowlingGame.roll(pinsDown);
-    expect(bowlingGame.rolls[0]).toEqual(6);
-  });
-
   it("counts pins down in multiple rolls", () => {
     const pinsDown = 6;
     const bowlingGame = new BowlingGame();
 
     bowlingGame.roll(pinsDown);
     bowlingGame.roll(pinsDown);
+    bowlingGame.roll(pinsDown);
     expect(bowlingGame.rolls[0]).toEqual(6);
     expect(bowlingGame.rolls[1]).toEqual(6);
+    expect(bowlingGame.rolls[2]).toEqual(6);
   });
 });
